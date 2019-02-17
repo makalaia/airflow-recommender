2d5b7f1daf912b967b4fc7690acaeb328317119d
makalaia <lucas.silva@sankhya.com.br>
2019-02-17 17:59:34 -0300

    Added test task Minor changes to pathing Update README.md

--- README.md
+++ README.md
@@ -1,8 +1,47 @@
 # AIRFLOW RECOMMENDER PIPELINE
 
-## Setup
-The easiest way to setup this project is to open this project directory and
-run the docker-compose file based on the great airflow image provied by puckel.
+This repository consists of a simplified recommender pipeline using the Apache Airflow framework.
+Although is not the focus of this work, the recommendation engine was based on the great LightFM library.
+And the dataset used for the tests was the movielens dataset (the 100k ratings version) previously parsed into a sparse COO matrix format.
 
-For this, just run the script **run_airflow_container.sh**.
+The dataset is composed of roughly 90k ratings for the training set, and 10k ratings for the test set.
+The training set was randomized and evenly distributed in 10 sparse matrices.
+
+To make the data transfer simpler, all the data transfer is file-based, and is stored on the s3 folder, to simulate 
+the AWS S3 service, which is basically a cloud file system. The same results could be obtained using the boto3 library
+in conjunction with the AWS S3 service, or any data source.
+
+## Tasks structure
+To simulate the passage of time, the start of the DAG was set by default to 10 days ago of the running date of the DAG. 
+Each new day would represent the addition of a new split of the training data being added to the model. 
+
+This way, we're trying to simulate the **addition of new data** and the trigger of a job to:
+- Extract this new data;
+- Retrain the current model with the new data;
+- Test the model performance;
+- Publish the results;
+
+With the addition of new data we expect the performance of the model to increase, which can be seen, after all the tasks
+ended on the file:
+```
+s3/output_data/performance.csv
+```
+
+## Setup
+The easiest way to setup this project is using docker-compose. To do so, open this project directory and
+run the docker-compose file based on the great [airflow image provided by puckel](https://github.com/puckel/docker-airflow).
+
+For this, just run the script **run_airflow_container.sh**, which, in addition of setting the container up,
+will also set the write permissions needed on the volumes.
+
+Then after, the airflow webserver will start on [localhost:8080](http://localhost:8080/), you just need to turn the DAG on, 
+and wait for all of the runs to be executed.
+
+### Windows
+If you are on windows, remember to set the PWD environment variable to the current directory before running 
+"docker-compose up":
+```
+SET PWD=%cd%
+docker-compose up
+```
 
--- dags/recommender_pipeline_dag.py
+++ dags/recommender_pipeline_dag.py
@@ -25,10 +25,9 @@ default_args = {
     'retry_delay': timedelta(minutes=2)
 }
 
-time_step = 1
-input_path = '/usr/local/airflow/s3/input_data/data.pickle'
-output_path = '/usr/local/airflow/s3/output_data/output_train.pickle'
-model_path = '/usr/local/airflow/s3/model/recommender_model.pickle'
+input_file_path = '/usr/local/airflow/s3/input_data/data.pickle'
+output_path = '/usr/local/airflow/s3/output_data/'
+model_file_path = '/usr/local/airflow/s3/model/recommender_model.pickle'
 dag = DAG('recommender_pipeline_dag', default_args=default_args, schedule_interval='@daily')
 
 
@@ -38,7 +37,7 @@ def extract_data(**context):
     time_step = (context['task_instance'].execution_date - default_args['start_date']).days + 1
     logging.info('TIME_STEP: %d' % time_step)
 
-    with open(input_path, mode='rb') as input_file:
+    with open(input_file_path, mode='rb') as input_file:
         movielens = pickle.load(input_file)
         train = movielens['train'][:time_step]
         shape = movielens['shape']
@@ -50,13 +49,13 @@ def extract_data(**context):
         logging.info('NNZ: %d' % train_data.nnz)
 
         create_dir(output_path)
-        with open(output_path, mode='wb') as output_file:
+        with open(output_path+'output_train.pickle', mode='wb') as output_file:
             pickle.dump(train_data, output_file)
             path = output_file.name
 
     # send the path for the next task
     context['task_instance'].xcom_push(key='time_step', value=time_step)
-    return path
+    context['task_instance'].xcom_push(key='path', value=path)
 
 
 extract_task = PythonOperator(
@@ -70,12 +69,12 @@ extract_task = PythonOperator(
 # Train task
 def train_model(**context):
     epochs = 10
-    path = context['task_instance'].xcom_pull(task_ids='extract_task')
+    path = context['task_instance'].xcom_pull(key='path', task_ids='extract_task')
     with open(path, mode='rb') as file:
         train_interactions = pickle.load(file)
     recommender = Recommender()
     recommender.fit(interactions=train_interactions, epochs=epochs)
-    recommender.dump_model(model_path)
+    recommender.dump_model(model_file_path)
 
 
 train_task = PythonOperator(
@@ -86,38 +85,45 @@ train_task = PythonOperator(
 )
 
 
-# Publish task
-def publish_model():
-    with open(model_path, mode='rb') as file:
+# Test task
+def test_model(**context):
+    with open(model_file_path, mode='rb') as file:
         model = pickle.load(file)
-        with open(input_path, mode='rb') as input_file:
+        with open(input_file_path, mode='rb') as input_file:
             data = pickle.load(input_file)
             test_data = data['test']
             auc = model.evaluate_auc(test_data)
-            mak = model.evaluate_at_k(test_data, 10)
+            pre_at_10 = model.evaluate_at_k(test_data, 10)
+            context['task_instance'].xcom_push(key='metrics', value=(auc, pre_at_10))
 
-            logging.info('TEST AUC: %.2f:' % auc)
-            logging.info('TEST PRECISION_AT_10: %.2f:' % mak)
+
+test_task = PythonOperator(
+    task_id='test_task',
+    python_callable=test_model,
+    provide_context=True,
+    dag=dag
+)
+
+
+# Publish task
+def publish_model(**context):
+    time_step = context['task_instance'].xcom_pull(key='time_step', task_ids='extract_task')
+    auc, pre_at_10 = context['task_instance'].xcom_pull(key='metrics', task_ids='test_task')
+
+    logging.info('TEST AUC: %.2f:' % auc)
+    logging.info('TEST PRECISION_AT_10: %.2f:' % pre_at_10)
+    with open(output_path + 'performance.csv', mode='a') as result_file:
+        result_file.write('{time_step},{auc},{pre_at_10}\n'.format(time_step=time_step, auc=auc, pre_at_10=pre_at_10))
 
 
 publish_task = PythonOperator(
     task_id='publish_task',
     python_callable=publish_model,
+    provide_context=True,
     dag=dag,
 )
 
 # Define graph
 train_task.set_upstream(extract_task)
-publish_task.set_upstream(train_task)
-
-if __name__ == "__main__":
-    DEFAULT_DATE = datetime.now()
-    DEFAULT_DATE = DEFAULT_DATE.replace(tzinfo=pytz.utc)
-    logging.info(DEFAULT_DATE)
-    logging.info('START')
-    # dag.run()
-    extract_task.run(start_date=DEFAULT_DATE, end_date=DEFAULT_DATE)
-    train_task.run(start_date=DEFAULT_DATE, end_date=DEFAULT_DATE)
-    publish_task.run(start_date=DEFAULT_DATE, end_date=DEFAULT_DATE)
-    logging.info('END')
-
+test_task.set_upstream(train_task)
+publish_task.set_upstream(test_task)
